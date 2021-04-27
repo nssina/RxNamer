d0792cbd7c92df9eadc36045f194cb8b7a6e0c2e
nssina <rabie_sina@yahoo.com>
2021-04-27 15:48:49 +0200

    Build UI

--- RxNamer/ViewController.swift
+++ RxNamer/ViewController.swift
@@ -6,9 +6,17 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var nameEntryTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var namesLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
