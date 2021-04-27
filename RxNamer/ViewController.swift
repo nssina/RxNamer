3545d6d0a3d7c5dcc6cdec9027da88ef37608c25
nssina <rabie_sina@yahoo.com>
2021-04-27 15:29:17 +0200

    Project started

---
+++ RxNamer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxNamer
+//
+//  Created by Sina Rabiei on 4/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
