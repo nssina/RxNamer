1a0c6bc90f9b009662908a1506383aae10718544
nssina <rabie_sina@yahoo.com>
2021-04-27 17:25:41 +0200

    Passing values between view controllers

--- RxNamer/ViewController.swift
+++ RxNamer/ViewController.swift
@@ -15,14 +15,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameEntryTextField: UITextField!
     @IBOutlet weak var submitButton: UIButton!
     @IBOutlet weak var namesLabel: UILabel!
+    @IBOutlet weak var addNameButton: UIButton!
     
     let disposeBag = DisposeBag()
-    var namesArray: [String] = []
+    var namesArray = BehaviorRelay<[String]>(value: [])
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         bindTextField()
         bindSubmitButton()
+        bindAddNameButton()
+        
+        namesArray.asObservable().subscribe(onNext: { names in
+            self.namesLabel.text = names.joined(separator: ", ")
+        }).disposed(by: disposeBag)
     }
     
     func bindTextField() {
@@ -43,11 +50,24 @@ class ViewController: UIViewController {
         submitButton.rx.tap
             .subscribe(onNext: {
                 if self.nameEntryTextField.text != "" {
-                    self.namesArray.append(self.nameEntryTextField.text!)
-                    self.namesLabel.rx.text.onNext(self.namesArray.joined(separator: ", "))
+                    self.namesLabel.rx.text.onNext(self.namesArray.value.joined(separator: ", "))
+                    self.namesArray.accept(self.namesArray.value + [self.nameEntryTextField.text!])
                     self.nameEntryTextField.rx.text.onNext("")
                     self.helloLabel.rx.text.onNext("Type your name below")
                 }
+            }).disposed(by: disposeBag)
+    }
+    
+    func bindAddNameButton() {
+        addNameButton.rx.tap
+            .throttle(RxTimeInterval.milliseconds(500), scheduler: MainScheduler.instance).subscribe(onNext: {
+                guard let addNameVC = self.storyboard?.instantiateViewController(identifier: "AddNameVC") as? AddNameVC else { return }
+                addNameVC.nameSubject
+                    .subscribe(onNext: { name in
+                        self.namesArray.accept(self.namesArray.value + [name])
+                        addNameVC.dismiss(animated: true, completion: nil)
+                    }).disposed(by: self.disposeBag)
+                self.present(addNameVC, animated: true, completion: nil)
             }).disposed(by: disposeBag)
     }
 }
